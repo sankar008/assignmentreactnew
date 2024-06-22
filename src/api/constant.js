f29e9644ffaccf782eed66a875469c7f5ea573ca
sankar bera <sankar.sb008@gmail.com>
2024-06-22 12:14:56 +0530

    remove backup file and change api port

--- src/api/constant.js
+++ src/api/constant.js
@@ -5,7 +5,7 @@ export const IMG = "http://document.apibag.in/";
 // ? ====== BASE URL ======
 //export const SOCEKT = "http://192.168.1.182:3001";
 export const SOCEKT = "";
-export const URL = "http://document.apibag.in:3001/v1";
+export const URL = "http://document.apibag.in:3002/v1";
 //export const URL = "http://192.168.1.182:3001/v1";
 
 export const STUDENT = `${URL}/student`;
